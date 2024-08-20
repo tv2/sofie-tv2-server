848343d18d9b47c24d4baa828be2215e84cdd299
Anders Frederik Jørgensen <afjo@tv2.dk>
2024-08-20 07:51:29 +0200

    fix: Moves proxy server configuration to start method to avoid async constructor flow.

--- src/fastify-server.ts
+++ src/fastify-server.ts
@@ -9,21 +9,20 @@ export interface ProxyConfiguration {
 export class FastifyServer implements ProxyServer {
   private readonly fastifyServer: fastify.FastifyInstance = createFastifyServer()
 
-  public constructor(private readonly proxyConfiguration: ProxyConfiguration) {
-    this.configureProxy()
+  public constructor(private readonly proxyConfiguration: ProxyConfiguration) {}
+
+  public async start(port: number): Promise<void> {
+    await this.configureProxy()
+    await this.fastifyServer.listen({ port })
+    console.log(`Running proxy server on port ${port} redirecting HTTP requests to ${this.proxyConfiguration.httpUrl} and WebSocket connections to ${this.proxyConfiguration.websocketUrl}.`)
   }
 
-  private configureProxy(): void {
-    this.fastifyServer.register(fastifyHttpProxy, {
+  private async configureProxy(): Promise<void> {
+    await this.fastifyServer.register(fastifyHttpProxy, {
       upstream: this.proxyConfiguration.httpUrl,
       websocket: true,
       wsUpstream: this.proxyConfiguration.websocketUrl,
     })
-  }
-
-  public async start(port: number): Promise<void> {
-    await this.fastifyServer.listen({ port })
-    console.log(`Running proxy server on port ${port} redirecting HTTP requests to ${this.proxyConfiguration.httpUrl} and WebSocket connections to ${this.proxyConfiguration.websocketUrl}.`)
   }
 
   public async stop(): Promise<void> {
