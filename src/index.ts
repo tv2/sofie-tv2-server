d88d5bdd5199b1fcda6fcfb5ea06bb13de8158b5
Anders Frederik Jørgensen <afjo@tv2.dk>
2024-08-20 07:49:37 +0200

    fix: Allows no tests and enables skipLibCheck.

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,8 @@ const SOFIE_WEBSOCKET_URL: string = 'ws://localhost:3006'
 const PROXY_SERVER_PORT: number = 3010
 
 async function configureSystemServices(): Promise<void> {
-  const proxyServer: ProxyServer = new FastifyServer(PROXY_SERVER_PORT, { httpUrl: SOFIE_REST_URL, websocketUrl: SOFIE_WEBSOCKET_URL })
-  await proxyServer.start()
+  const proxyServer: ProxyServer = new FastifyServer({ httpUrl: SOFIE_REST_URL, websocketUrl: SOFIE_WEBSOCKET_URL })
+  await proxyServer.start(PROXY_SERVER_PORT)
 }
 
 // TODO: Add logger
