0ca0cc7e272edbff0738fdd34b63dbc3d74da94c
Anders Frederik Jørgensen <afjo@tv2.dk>
2024-08-20 07:37:23 +0200

    feat: Adds HTTP and WebSocket proxy server.
    SOF-1869: HTTP proxy server.
    SOF-1871: WebSocket proxy server.

---
+++ src/fastify-server.ts
@@ -0,0 +1,32 @@
+import { ProxyServer } from './interfaces/proxy-server'
+import createFastifyServer, * as fastify from 'fastify'
+import { fastifyHttpProxy } from '@fastify/http-proxy'
+
+export interface ProxyConfiguration {
+  httpUrl: string
+  websocketUrl: string
+}
+export class FastifyServer implements ProxyServer {
+  private readonly fastifyServer: fastify.FastifyInstance = createFastifyServer()
+
+  public constructor(private readonly port: number, private readonly proxyConfiguration: ProxyConfiguration) {
+    this.configureProxy()
+  }
+
+  private configureProxy(): void {
+    this.fastifyServer.register(fastifyHttpProxy, {
+      upstream: this.proxyConfiguration.httpUrl,
+      websocket: true,
+      wsUpstream: this.proxyConfiguration.websocketUrl,
+    })
+  }
+
+  public async start(): Promise<void> {
+    await this.fastifyServer.listen({ port: this.port })
+    console.log(`Running proxy server on port ${this.port} redirecting HTTP requests to ${this.proxyConfiguration.httpUrl} and WebSocket connections to ${this.proxyConfiguration.websocketUrl}.`)
+  }
+
+  public async stop(): Promise<void> {
+    await this.fastifyServer.close()
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
-import { NameGenerator } from './name-generator'
+import { ProxyServer } from './interfaces/proxy-server'
+import { FastifyServer } from './fastify-server'
 
-const nameGenerator: NameGenerator = new NameGenerator()
+const SOFIE_REST_URL: string = 'http://localhost:3005'
+const SOFIE_WEBSOCKET_URL: string = 'ws://localhost:3006'
+const PROXY_SERVER_PORT: number = 3010
 
-console.log(`Welcome to ${nameGenerator.getName()}!`)
+async function configureSystemServices(): Promise<void> {
+  const proxyServer: ProxyServer = new FastifyServer(PROXY_SERVER_PORT, { httpUrl: SOFIE_REST_URL, websocketUrl: SOFIE_WEBSOCKET_URL })
+  await proxyServer.start()
+}
+
+// TODO: Add logger
+configureSystemServices().catch(console.error)
