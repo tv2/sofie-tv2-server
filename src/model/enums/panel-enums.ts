996b717731aa5e707ddd15a9d781c755ffcf9f8c
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2024-09-24 10:00:20 +0200

    SOF-2225 Add PhysicalPanelLayout interface and map Skaarhoj.MKT1A and Skaarhoj.MK48

---
+++ src/model/enums/panel-enums.ts
@@ -0,0 +1,18 @@
+export enum PanelType {
+  SKAARHOJ = 'SKAARHOJ',
+}
+
+export type PanelModel = SkaarhojModel
+
+export enum SkaarhojModel {
+  MK48 = 'SK_MK48',
+  MKT1A = 'SK_MKT1A',
+}
+
+export enum InputType {
+  BUTTON = 'BUTTON',
+  BUTTON_WITH_DISPLAY = 'BUTTON_WITH_DISPLAY',
+  DISPLAY = 'DISPLAY',
+  LED_DISPLAY = 'LED_DISPLAY',
+  FADER = 'FADER',
+}
