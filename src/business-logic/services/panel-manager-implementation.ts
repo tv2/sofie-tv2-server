82b3bef7b06d0e36ea119076161da36e7cebd2d7
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2024-10-28 15:05:56 +0100

    SOF-2248 Connect to the Panel when a new PanelConfiguration is created

--- src/business-logic/services/panel-manager-implementation.ts
+++ src/business-logic/services/panel-manager-implementation.ts
@@ -4,6 +4,9 @@ import { PanelConfigurationRepository } from '../../data-access/interfaces/panel
 import { PanelConfiguration } from '../../model/interfaces/panel-configuration'
 import { PanelFactory } from '../panel-integrations/panel-factory'
 import { Panel } from './interfaces/panel'
+import { PanelEventObserver } from '../../presentation/interfaces/panel-event-observer'
+import { PanelConfigurationCreatedEvent, PanelEvent } from '../../presentation/value-objects/panel-event'
+import { PanelEventType } from '../../presentation/enums/event-type'
 
 export class PanelManagerImplementation implements PanelManager {
   private readonly logger: Logger
@@ -11,9 +14,20 @@ export class PanelManagerImplementation implements PanelManager {
   public constructor(
     private readonly panelFactory: PanelFactory,
     private readonly panelConfigurationRepository: PanelConfigurationRepository,
+    private readonly panelEventObserver: PanelEventObserver,
     logger: Logger
   ) {
     this.logger = logger.tag(PanelManagerImplementation.name)
+
+    this.panelEventObserver.subscribeToPanelEvents((panelEvent: PanelEvent) => {
+      if (this.isPanelConfigurationCreatedEvent(panelEvent)) {
+        this.connectToPanel(panelEvent.panelConfiguration)
+      }
+    })
+  }
+
+  private isPanelConfigurationCreatedEvent(panelEvent: PanelEvent): panelEvent is PanelConfigurationCreatedEvent {
+    return panelEvent.type === PanelEventType.PANEL_CONFIGURATION_CREATED
   }
 
   public async initialize(): Promise<void> {
@@ -26,9 +40,11 @@ export class PanelManagerImplementation implements PanelManager {
 
   private async connectToPanels(): Promise<void> {
     const panelConfigurations: PanelConfiguration[] = await this.panelConfigurationRepository.getPanelConfigurations()
-    panelConfigurations.map((panelConfiguration) => {
-      const panel: Panel = this.panelFactory.createPanel(panelConfiguration)
-      panel.initialize()
-    })
+    panelConfigurations.map(this.connectToPanel.bind(this))
+  }
+
+  private connectToPanel(panelConfiguration: PanelConfiguration): void {
+    const panel: Panel = this.panelFactory.createPanel(panelConfiguration)
+    panel.initialize()
   }
 }
