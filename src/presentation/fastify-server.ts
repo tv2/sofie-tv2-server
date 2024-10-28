d257ff526633dd17461ce3f56ffd3e55a59fb32e
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2024-10-28 08:22:21 +0100

    SOF-2247 Remove disable eslint comment

--- src/presentation/fastify-server.ts
+++ src/presentation/fastify-server.ts
@@ -45,17 +45,14 @@ export class FastifyServer implements ProxyServer {
     })
   }
 
-  // The below eslint-disable is necessary.
-  // If we add 'await' or .then().catch() we will get a 'MaxListenerExceededWarning: Possible EventEmitter memory leak detected.'
-  /* eslint-disable @typescript-eslint/no-floating-promises */
   private addCors(): void {
     this.fastifyServer.addHook('onRequest', async(request, reply) => {
       reply.header('Access-Control-Allow-Origin', '*')
-      reply.header('Access-Control-Allow-Credentials', true)
-      reply.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept, X-Slug, X-UID')
-      reply.header('Access-Control-Allow-Methods', 'OPTIONS, POST, PUT, PATCH, GET, DELETE')
+        .header('Access-Control-Allow-Credentials', true)
+        .header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept, X-Slug, X-UID')
+        .header('Access-Control-Allow-Methods', 'OPTIONS, POST, PUT, PATCH, GET, DELETE').then(() => {}, () => {})
       if (request.method === 'OPTIONS') {
-        reply.send()
+        reply.send().then(() => {}, () => {})
       }
     })
   }
