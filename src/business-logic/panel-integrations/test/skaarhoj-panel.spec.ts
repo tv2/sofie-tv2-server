7052ed1a3cf67a9a2d8b76c26e559be0f7022a4a
Dan Vu <davu@tv2.dk>
2025-05-15 14:37:05 +0200

    Sof 2572/made t bar value spike detection for faulty skaarhoj panel (#123)
    * Added t-bar value spike detection
    
    * Fixed lint errors
    
    * changed error message
    
    * Fix linter errors

--- src/business-logic/panel-integrations/test/skaarhoj-panel.spec.ts
+++ src/business-logic/panel-integrations/test/skaarhoj-panel.spec.ts
@@ -1,12 +1,19 @@
 import { SkaarhojPanel } from '../skaarhoj-panel'
 import { PanelConfiguration } from '../../../model/interfaces/panel-configuration'
 import { EntityTestFactory } from '../../../model/test/entity-test-factory'
-import { PanelModel, PanelType, SkaarhojModel } from '../../../model/enums/panel-enums'
+import { PanelCommandType, PanelModel, PanelType, SkaarhojModel } from '../../../model/enums/panel-enums'
 import { UnsupportedOperationException } from '../../../model/exceptions/unsupported-operation-exception'
 import { Logger } from '../../../logger/logger'
 import { instance, mock } from '@typestrong/ts-mockito'
 import { StatusMessageService } from '../../services/status-message-service'
 import { ColorConverter } from '../../interfaces/color-converter'
+import { TBarPanelCommand, PanelCommand } from '../../../model/interfaces/input-configuration'
+
+class FakeSkaarhojPanel extends SkaarhojPanel {
+  public callUpdateTBarCommandWithValues(command: TBarPanelCommand, value: number): PanelCommand {
+    return this.updateTBarCommandWithValues(command, value)
+  }
+}
 import { InvokedActionService } from '../../interfaces/invoked-action-service'
 
 describe(SkaarhojPanel.name, () => {
@@ -39,6 +46,28 @@ describe(SkaarhojPanel.name, () => {
           new SkaarhojPanel(panelConfiguration, instance(mock<InvokedActionService>()), instance(mock(StatusMessageService)), instance(mock<ColorConverter>()), logger, EntityTestFactory.createPanelLayoutConfiguration())
         }).not.throws()
       })
+    })
+  })
+
+  describe('SkaarhojPanel receives T-bar value 0 then 1000', () => {
+    it('throws a TBarValueSpikeException', () => {
+      panelConfiguration = EntityTestFactory.createPanelConfiguration({ type: PanelType.SKAARHOJ, model: SkaarhojModel.MKT1A })
+      const panel = new FakeSkaarhojPanel(panelConfiguration, instance(mock<InvokedActionService>()), instance(mock(StatusMessageService)), instance(mock<ColorConverter>()), logger, EntityTestFactory.createPanelLayoutConfiguration())
+      expect(() => {
+        panel.callUpdateTBarCommandWithValues({ type: PanelCommandType.T_BAR, shouldExecuteTake: false, value: 0 }, 0)
+        panel.callUpdateTBarCommandWithValues({ type: PanelCommandType.T_BAR, shouldExecuteTake: false, value: 0 }, 1000)
+      }).throws()
+    })
+  })
+
+  describe('SkaarhojPanel receives T-bar value 1000 then 0', () => {
+    it('throws a TBarValueSpikeException', () => {
+      panelConfiguration = EntityTestFactory.createPanelConfiguration({ type: PanelType.SKAARHOJ, model: SkaarhojModel.MKT1A })
+      const panel = new FakeSkaarhojPanel(panelConfiguration, instance(mock<InvokedActionService>()), instance(mock(StatusMessageService)), instance(mock<ColorConverter>()), logger, EntityTestFactory.createPanelLayoutConfiguration())
+      expect(() => {
+        panel.callUpdateTBarCommandWithValues({ type: PanelCommandType.T_BAR, shouldExecuteTake: false, value: 0 }, 1000)
+        panel.callUpdateTBarCommandWithValues({ type: PanelCommandType.T_BAR, shouldExecuteTake: false, value: 0 }, 0)
+      }).throws()
     })
   })
 })
