99b26686016eb7e3e5e97e10e71be896c6dd313d
Anders Frederik Lou Jørgensen <afjo@tv2.dk>
2025-07-17 13:41:31 +0200

    fix: Excludes migrations package from root tsc and eslint.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,6 @@ import tv2MediaEslintConfig from '@tv2media/eslint-config'
 export default [
   ...tv2MediaEslintConfig,
   {
-    ignores: ['.yarn/**', 'coverage/**', 'src/migrations/mongo/mongo-migrations/*.ts', 'database/**/*.ts'],
+    ignores: ['.yarn/**', 'coverage/**', 'src/migrations/mongo/mongo-migrations/*.ts', 'database/**/*.ts', "migrations/**"],
   },
 ]
